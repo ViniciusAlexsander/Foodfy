c2b2e01290c996a85a3449713fcd4edf40b5450f
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2020-04-05 15:03:37 -0300

    Comecei a aplicar o site juntamente com um backend, falta arrumar o acesso as rotas para a receita individual

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const server = express();
+const receitas = require("./data");
+
+server.use(express.static("public"));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true,
+});
+
+server.get("/", (req, res) => {
+  return res.render("home", { items: receitas });
+});
+
+server.get("/sobre", (req, res) => {
+  return res.render("sobre");
+});
+
+server.get("/receitas", (req, res) => {
+  return res.render("receitas", { items: receitas });
+});
+
+server.get("/receita/:index", (req, res) => {
+  const recipes = receitas;
+  const recipeIndex = req.params.index;
+
+  return res.render("receita", { receita: recipes[recipeIndex] });
+});
+
+server.listen(1234, () => {
+  console.log(" O pai ta on na porta 1234");
+});
